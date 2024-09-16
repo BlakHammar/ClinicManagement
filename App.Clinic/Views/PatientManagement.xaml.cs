04dad9ae9582e5f275c7223359dbec4de1ba4dfc
BlakHammar <MixedTwinzs@gmail.com>
2024-09-16 00:36:39 -0400

    First pass of an add patient screen

--- App.Clinic/Views/PatientManagement.xaml.cs
+++ App.Clinic/Views/PatientManagement.xaml.cs
@@ -1,10 +1,18 @@
 using Library.Clinic.Models;
 using Library.Clinic.Services;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace App.Clinic.Views;
 
-public partial class PatientManagement : ContentPage
+public partial class PatientManagement : ContentPage, INotifyPropertyChanged
 {
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    private void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
     public List<Patient> Patients
     {
         get
@@ -21,5 +29,10 @@ public partial class PatientManagement : ContentPage
     private void CancelClicked(object sender, EventArgs e)
     {
         Shell.Current.GoToAsync("//MainPage");
+    }
+
+    private void AddClicked(object sender, EventArgs e)
+    {
+        Shell.Current.GoToAsync("//PatientDetails");
     }
 }
